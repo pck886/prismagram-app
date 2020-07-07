c5cc248c96f5053509ee9d8cf324585eac35ba54
chanlee <pck886@gmail.com>
2020-07-08 05:45:15 +0900

    Initial Commit

---
+++ apollo.js
@@ -0,0 +1,2 @@
+const apolloClientOptions = { uri: "http://localhost:4000/graphql" };
+export default apolloClientOptions;
---
+++ screens/Auth/AuthHome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const View = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+
+const Text = styled.Text``;
+
+export default ({ navigation }) => (
+  <Text>Go to Login</Text>
+);
